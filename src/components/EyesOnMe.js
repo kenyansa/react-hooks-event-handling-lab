42c53d380d02e243c99735533860d27825ed3221
kenyansa <kenyansajustine@gmail.com>
2022-05-10 10:32:42 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+function EyesOnMe(){
+    function isFocus(){
+        console.log("Good!")
+    }
+    function isBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <button onFocus={isFocus} onBlur={isBlur} >Eyes on me
+        </button>
+    )
+        
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function Keypad(){
+    function detectInput(){
+    console.log("Entering password...")
+    }
+    return (
+        <input type="password" onChange={detectInput} />
+        );
+}
+export default Keypad;
